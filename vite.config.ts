bb78822e0dd77e2ea34f4f8b19812db0a0454b35
Spiritux <spirituxebook@gmail.com>
2026-01-31 23:36:57 +0100

    Update Vite config for chunk size and code splitting
    Increased chunk size warning limit and optimized code splitting.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -8,6 +7,20 @@ export default defineConfig({
     'process.env': {
       API_KEY: JSON.stringify(process.env.API_KEY),
       NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }
+  },
+  build: {
+    // 1. Augmente la limite de l'avertissement à 1000kb
+    chunkSizeWarningLimit: 1000,
+    // 2. Optimise la séparation du code (Code Splitting)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
     }
   }
 });
